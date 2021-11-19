4849a2ba33d877c2ceb4b5730b37f10e63a5a74b
LittleCai <2243157861@qq.com>
2021-11-19 14:50:35 +0800

    Initial commit

---
+++ src/main/java/com/csl/emergencymaterialms/EmergencyMaterialMsApplication.java
@@ -0,0 +1,13 @@
+package com.csl.emergencymaterialms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyMaterialMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyMaterialMsApplication.class, args);
+    }
+
+}
